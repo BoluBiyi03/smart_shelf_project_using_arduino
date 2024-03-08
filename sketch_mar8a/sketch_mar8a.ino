ebdd155353f908815bc7fcfeae7820cb65d5decd
unknown <objas9@gmail.com>
2024-03-08 11:22:34 +0100

    Modify the README.md file and also the source code

--- sketch_mar8a/sketch_mar8a.ino
+++ sketch_mar8a/sketch_mar8a.ino
@@ -1,28 +1,4 @@
-# smart_shelf_project_using_arduino
-## Date 
-8th March,2024
-## By
-[Oluwabiyi Boluwatife Joshua](https://github.com/BoluBiyi03)
-## Introduction
- In embedded systems the current worldwide trend is ubiquitous computing. It enables communication between everyday objects by embedding microprocessors in everyday objects to make our life simpler. Thisparticular idea revolves around sensing the load of the shelves thereby detecting the level of stocks and transmitting it to the user's mobile device. We extend this concept to solve everyday problems like forgetting to purchase a particular grocery item. Devices like smartphones and wearables keep us reachable, interactable and updated to the everyday events happening around the world. We target to embed a mind in shelves, utilizing sensor and smartphone technology an automated to buy list is generated. To implement such a system,there is the need to continuously fetch,elaborate, and analyze data deriving from smart objects. This is achieved by the use ofSensor Networks, consisting of ArduinoUno along with HC- SR04 ultrasonic sensor interface. An ultrasonic sensor is used to read the level of goods on a shelf which uses the time of flight of an ultrasonic pulse to determine the distance.
- ## Problem Statement
- To develop a system that senses the level or weight of stocks and goods in the organization warehouse incase of less stock and to be able to generate alerts and share information with users connected on mobile devices.
- ## Required Hardware
- 1) Arduino Uno
- 2) Button
- 3) Potentiometer
- 4) LED
- 5) Resistors
- 6) Capacitor
- 7) LCD Display
- 8) Buzzer
- 9) Power or Battery
- 10) LM34
- ## Schematic Capture
- [schematic design](https://drive.google.com/file/d/1x-WJq2eqVkdl4yezWi42FByaNasgCgNu/view?usp=sharing)
- ## Source Code
- ``` cpp
- #include <SoftwareSerial.h>// Importing SoftwareSerial.h as a library
+#include <SoftwareSerial.h>// Importing SoftwareSerial.h as a library
 #include <LiquidCrystal.h>// Importing LiquidCrystal.h as a library
 
 SoftwareSerial mySerial(0, 1);//Setting pin 0 and pin 1 as TX and RX respectively
@@ -129,12 +105,3 @@ void receiveTextMessage()
   sim.println("AT+CNMI=2,2,0,0,0");// AT command to receive a live sms 
   delay(1000);// Delaying for 1 seconds 
 }
-```
-## HEX File
-[text]([text](sketch_mar8a/sketch_mar8a.ino.hex))
-## Advantage
-The system provides access to authorized persons only. It generates alerts based on exhaustion of stocks. This system is real time and user friendly
-## Disadvantage
-The seamless working of the product largely depends upon the network signal quality. Calibration of the ultrasonic sensors can be a deployment time overhead. Also the system is expensive to scale up.
-## Summary and Conclusion 
-In summary, a smart shelf demonstrates the power of IoT technology and microcontroller-based systems to enhance everyday processes and provide valuable real-time information. It can find applications in homes, retail stores, warehouses, and various other settings where efficient item monitoring and management are crucial. Building a smart shelf project not only offers a hands-on learning experience but also has the potential to solve practical problems and streamline processes in a smart and innovative way.
